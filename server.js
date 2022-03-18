3d335040fc5275755350dddfbf851d2071e4730e
Manpreet <preetr4ha@gmail.com>
2022-03-18 15:52:45 -0600

    make it better

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const app = express();
 const connectDB = require("./utils/connectDB");
 const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
+const path = require("path");
+
 // spells
 require("dotenv").config();
 require("express-async-errors");
 app.use(cors());
+
+// server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/bookmark/build")); // set static folder
+  //returning frontend for any route other than api
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "bookmark", "build", "index.html")
+    );
+  });
+}
 
 // routers
 const bookmarkRouter = require("./routes/bookmark");
