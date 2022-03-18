c5013a106c28e456d27953ccd32afad1f60c69a8
Manpreet <preetr4ha@gmail.com>
2022-03-18 16:02:04 -0600

    t

--- client/bookmark/src/context/ContextProvider.js
+++ client/bookmark/src/context/ContextProvider.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 // Base url
-const baseUrl = "http://localhost:5000/";
+const baseUrl = "/";
 const BookmarkContext = React.createContext();
 function BookmarkProvider({ children }) {
   const [bookmarks, setBookmarks] = useState([]);
