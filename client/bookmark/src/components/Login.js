1679dbb7922724352713a7515e18e78931d67fca
Manpreet <manpreetrahal27@gmail.com>
2022-10-15 03:11:40 -0600

    Fix: Netlify router redirect

--- client/bookmark/src/components/Login.js
+++ client/bookmark/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import {baseUrl} from "../info";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -9,7 +10,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/user/login", {
+      const res = await axios.post(`${baseUrl}/user/login`, {
         email,
         password,
       });
